8ce544a5498b94114b017258c45857b61f140b65
ThePrakharRajpali <rprakhar@iitg.ac.in>
2021-01-05 17:39:27 +0530

    removed excess codes and models

--- models/branch.js
+++ models/branch.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 
 var branchSchema = new mongoose.Schema({
     name: String,
-    semester: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Semester",
-        }
-    ],
+    // semester: [
+    //     {
+    //         type: mongoose.Schema.Types.ObjectId,
+    //         ref: "Semester",
+    //     }
+    // ],
     subjects: [
         {
             type: mongoose.Schema.Types.ObjectId,
--- models/subject.js
+++ models/subject.js
@@ -3,25 +3,14 @@ const mongoose = require('mongoose');
 var subjectSchema = new mongoose.Schema({
     subjectCode: String, //code eg. CS221
     name: String, //official name eg. Digital design
-
-    // resources: [
-    //     {
-    //
-    //     }
-    // ]
+    branch: String,
+    semester: String,
+    semsterNum: Number,
     resources: [
         {
             type: String,
         }
     ],
-    branch: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Branch",
-    },
-    semester: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Semester",
-    },
 });
 
 module.exports = mongoose.model("Subject", subjectSchema);
